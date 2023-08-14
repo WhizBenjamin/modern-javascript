9a73413e28dacaed4c72e9c5ba9df89c0bf180eb
WhizBenjamin <kunwhizben@gmail.com>
2023-08-15 00:05:30 +0100

    Update sandbox.js
    variables, constants and comments

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,20 @@
-console.log(1);
-console.log(2);
+let age = 25;
+let year = 2023;
+
+console.log(age, year);
+
+age = 30; 
+/*  note: since the variable (age) had been previously declared,
+    there is no need to use 'let' in asigning it a new value.*/
+console.log(age);
+
+const points = 100;
+console.log(points);
+// const is another method to declare variables except that the values are constant.
+
+var score = 75;
+console.log(score);
+
+/*  note:    1. start your variables with a letter, an underscore or a $.
+             2. after that, use as many letters, numeric digits, underscores or $ as you like.
+             3. practice camel casing for variables with two or more words.*/
